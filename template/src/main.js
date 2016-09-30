8062f21ca165dc7c2cf6edafdf36c649241f238a
xing-zhi <xing_zhi@126.com>
2016-09-30 14:28:20 +0800

    vue router

--- template/src/main.js
+++ template/src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-}){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import router from './router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import rootComponent from './App.vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+const app = Vue.extend(rootComponent){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+router.start(app, 'app'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
